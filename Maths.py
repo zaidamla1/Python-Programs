7a23f7b655de4a5836757a440d645db76f4ecc11
zaid <kkzyxw@gmail.com>
2026-06-25 09:25:35 +0530

    Math function

---
+++ Maths.py
@@ -0,0 +1,31 @@
+import math
+
+x = math.ceil(26.53)
+print(x)
+
+y = math.floor(36.23)
+print(y)
+
+sqrt = math.sqrt(169)
+print(sqrt)
+
+cbrt = math.cbrt(125)
+print(cbrt)
+
+fact = math.factorial(5)
+print(fact)
+
+minVal = min(12,8,16,22,11,6,17)
+print(minVal)
+
+maxVal = max(11,18,12,16,1,22,19)
+print(maxVal)
+
+pi = math.pi
+print(pi)
+
+power = math.pow(5,4)
+print(power)
+
+reminder = math.remainder(10,3)
+print(reminder)
